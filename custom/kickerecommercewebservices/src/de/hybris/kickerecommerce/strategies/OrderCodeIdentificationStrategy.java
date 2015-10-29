d4035ad4c8a1977ead06e5c8b004dc1a88c9cb0e
gaurav <gaurav.asthana@accenture.com>
2015-10-29 14:32:37 +0530

    Revert "adding files"
    This reverts commit 97ff23d4261b753d15c0c5b4338b47d18c688429.

--- custom/kickerecommercewebservices/src/de/hybris/kickerecommerce/strategies/OrderCodeIdentificationStrategy.java
+++
@@ -1,9 +0,0 @@
-package de.hybris.kickerecommerce.strategies;
-
-
-/**
- * Strategy for identifying is given string is GUID.
- */
-public interface OrderCodeIdentificationStrategy {
-    boolean isID(String potentialId);
-}
